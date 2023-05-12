aac560e81f7e4921160b0125ffb6ef6005807078
Jinkun Chen <i@jinkunchen.com>
2023-05-12 13:23:09 -0300

    Initialize Maturin project to build the project to a python lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+use pyo3::prelude::*;
+
 mod converter;
 
-fn main() {
+#[pyfunction]
+fn processor(input_file_name: &str, output_file_name: &str) {
     // read read args
-    let args: Vec<String> = std::env::args().collect();
-    let input_file_name = &args[1];
-    let output_file_name = &args[2];
+    // let args: Vec<String> = std::env::args().collect();
+    // let input_file_name = &args[1];
+    // let output_file_name = &args[2];
 
     println!("Start Reading! (This may take a while)");
     let activations = converter::reader::Activation::from_file(&input_file_name);
@@ -13,4 +16,11 @@ fn main() {
     println!("Start Writing!(This may take a while)");
     converter::writer::to_w2v(&activations, &output_file_name);
     println!("Finish Writing!");
+}
+
+#[pymodule]
+fn concept_x_converter(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(processor, m)?)?;
+
+    Ok(())
 }
